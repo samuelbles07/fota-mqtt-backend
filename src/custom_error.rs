e93499215e64e0607e12257de9833eb62d90126e
Samuel Siburian <samuel.siburian@bobobox.com>
2024-06-17 00:06:19 +0700

    Add new error type for job

--- src/custom_error.rs
+++ src/custom_error.rs
@@ -4,12 +4,14 @@ use std::fmt;
 #[derive(Debug)]
 pub enum CustomError {
     HttpRequest(u16),
+    StartJob(String),
 }
 
 impl fmt::Display for CustomError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::HttpRequest(errcode) => write!(f, "Http request failed with code {errcode}"),
+            Self::StartJob(msg) => write!(f, "StartJob Failed ({})", msg),
         }
     }
 }
