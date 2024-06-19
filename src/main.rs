efeb24578fadbab960602796f2434bc97fbca17b
Samuel Siburian <samuel.siburian@bobobox.com>
2024-06-20 00:20:07 +0700

    Integrate http server mod
    channel between http server and jobs
    Run job in its own thread

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod custom_error;
 mod file_handler;
+mod httpserver;
 mod jobs;
 mod messenger;
 mod telemetry;
@@ -15,13 +16,18 @@ fn main() {
     pretty_env_logger::init();
     info!("Starting fota service");
 
-    // Create chanel for passing notification from messenger to jobs
+    // Create channel for passing notification from messenger to jobs thread
     let (tx_notification, rx_notification) = mpsc::channel();
+    // Create channel for passing new job from http server to jobs thread
+    let (tx_new_job, rx_new_job) = mpsc::channel();
 
     // Initialize messenger, it already handle mqtt connection on other thread
     let messenger = messenger::Messenger::new(tx_notification, "backend", "broker.emqx.io", 1883);
 
     // Initialize jobs and run
-    let mut jobs = jobs::JobScheduler::new(messenger, rx_notification);
+    let jobs = jobs::JobScheduler::new(messenger, rx_notification, rx_new_job);
     jobs.run();
+
+    let mut http = httpserver::HTTPServer::new("127.0.0.1", 7878, tx_new_job);
+    http.run();
 }
