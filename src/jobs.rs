02d2e43b9ccf23545ac75d768a7b39ef2c3b4190
Samuel Siburian <samuel.siburian@bobobox.com>
2024-06-22 23:05:34 +0700

    Fix hardcoded chunk id

--- src/jobs.rs
+++ src/jobs.rs
@@ -206,6 +206,9 @@ impl JobScheduler {
                 // Send fota request command to target device
                 trace!("data of {} => {:?}", job_id, chunk);
                 let tosend = telemetry::build_packet(&job.device_id, 123, chunk);
+                debug!("data of {} => {:?}", job_id, job.image.last_bytes_index);
+                let tosend =
+                    telemetry::build_packet(&job.device_id, job.image.last_bytes_index, chunk);
                 let _ = self.messenger.send(tosend); // TODO: Handle error
             }
             None => {
